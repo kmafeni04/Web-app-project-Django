2791ad774e611470dc944427f1fc7e85310e61db
kmafeni04 <88457139+kmafeni04@users.noreply.github.com>
2023-05-23 09:03:59 +0100

    Add files via upload

---
+++ web_app_hotel_app/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+
+# Create your models here.
+
+class Reservations(models.Model):
+    full_name = models.CharField(max_length=50)
+    email = models.EmailField(max_length=254)
+    CHOICES = (
+        ('Deluxe room', 'Deluxe room'),
+        ('Standard room', 'Standard room'),
+        ('Suite', 'Suite'),
+        ('Family room', 'Family room'),
+    )
+    room_type = models.CharField(max_length=50, choices = CHOICES)
+    check_in_date = models.DateField()
+    check_out_date = models.DateField()
+
+    def __str__(self):
+        return self.full_name
---
+++ web_app_hotel_app/urls.py
@@ -0,0 +1,33 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    #general logged out links
+    path('logout_user/', views.logout_user, name="logout_user"),
+    path('', views.index, name="index"),
+    path('baseloggedout/', views.baseloggedout, name="baseloggedout"),
+    path('loginselection/', views.loginselection, name="loginselection"),
+    path('signup/', views.signup, name="signup"),
+    path('guestlogin/', views.guestlogin, name="guestlogin"),
+    path('roomssignedout/', views.roomssignedout, name="roomssignedout"),
+    
+    #general logged in links
+    path('base/', views.base, name="base"),
+    path('home/', views.home, name="home"),
+    path('rooms/', views.rooms, name="rooms"),
+    path('reservations/', views.reservations, name="reservations"),
+    path('services/', views.services, name="services"),
+    path('contactguest/', views.contactguest, name="contactguest"),
+
+    #admin links
+    path('baseadmin/', views.baseadmin, name="baseadmin"),
+    path('adminlogin/', views.adminlogin, name="adminlogin"),
+    path('admincontrol/', views.admincontrol, name="admincontrol"),
+    path('adminguestcontrol/', views.adminguestcontrol, name="adminguestcontrol"),
+    path('roomsadmin/', views.roomsadmin, name="roomsadmin"),
+    path('reservationsadmin/', views.reservationsadmin, name="reservationsadmin"),
+    path('contactadmin/', views.contactadmin, name="contactadmin"),    
+
+]
+
